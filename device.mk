903585b5d3ce2828f9125fb4006ed1c628690d23
Bruno Martins <bgcngm@gmail.com>
2025-09-21 23:14:43 +0100

    pagani: Import previously decommonized UDFPS related parts
    Change-Id: Iee5894b319689e4ad0b7f211ce9e260544e4e839
    Signed-off-by: Robertgarciaa <chae0218@naver.com>

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ $(foreach display_id, 4630946916234099603 4630946983774026899 463094703957190285
         $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_$(display_id).xml \
     ))
 
+# Fingerprint
+$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
+
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
 
@@ -43,6 +46,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 $(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.oplus
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
