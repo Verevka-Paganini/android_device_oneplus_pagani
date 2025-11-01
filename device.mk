15d8dc510ecb78393d5d4e553a7698445678ab7c
Pranav Vashi <neobuddy89@gmail.com>
2025-11-01 17:04:37 +0530

    pagani: Enable UDFPS animations
    * crdroid feature
    
    Change-Id: Ic0c7bfed383d17f7badd7e7fbde6eb83be700369
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ $(foreach display_id, 4630946916234099603 4630946983774026899 463094703957190285
 # Fingerprint
 $(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
 
