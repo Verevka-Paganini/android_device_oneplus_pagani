a91e3ce967f4a13330a1a59073f58a8d1addb547
Pranav Vashi <neobuddy89@gmail.com>
2025-11-22 01:39:40 +0530

    dodge: Include LTPO settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ $(foreach display_id, 4630946916234099603 4630946983774026899 463094703957190285
         $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_$(display_id).xml \
     ))
 
+PRODUCT_PACKAGES += \
+    OplusLtpo
+
 # Fingerprint
 $(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
