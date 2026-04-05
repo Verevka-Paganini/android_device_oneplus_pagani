4958e0196240aef994af5e36f4989df212751e0c
z <z@ze.fyi>
2026-04-05 19:36:42 +0800

    feat(sepolicy): add private sepolicy rules and file contexts
    Signed-off-by: Robertgarciaa <chae0218@naver.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,5 +29,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
+# Sepolicy
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/pagani/BoardConfigVendor.mk
