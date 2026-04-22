4c24f306b87827ad547c7a1d5ecf25f71052273a
Robertgarciaa <chae0218@naver.com>
2026-04-22 23:11:56 +0900

    pagani: Initial conversion from dodge
    Update vendor blobs from COS 11.C.42
    Include oplusstanvbk from firmware list
    * Because I'll use Chinese version only!!
    
    Signed-off-by: Robertgarciaa <chae0218@naver.com>

---
+++ lineage_pagani.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021-2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from pagani device
+$(call inherit-product, device/oneplus/pagani/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_pagani
+PRODUCT_DEVICE := pagani
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := PKX110
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1772763728695 release-keys" \
+    BuildFingerprint=OnePlus/PKX110/OP60F5L1:16/AP3A.240617.008/V.193727b_99ecf4_960ada:user/release-keys \
+    DeviceName=OP60F5L1 \
+    DeviceProduct=PKX110 \
+    SystemDevice=OP60F5L1 \
+    SystemName=PKX110
