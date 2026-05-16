3ad53e008343ef700954bfbf1ef21e99a5aaebd9
Verevka <verevka86@gmail.com>
2026-05-16 10:13:18 +0300

    Revert "pagani: Include mindthegapps"
    This reverts commit 4ed08fad802fe0acbe4a40384e3094936b04d8fd.

--- lineage_pagani.mk
+++ lineage_pagani.mk
@@ -14,9 +14,6 @@ $(call inherit-product, device/oneplus/pagani/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Call mindthegapps
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
- 
 WITH_GAPPS := true
 TARGET_DISABLE_MATLOG := true
 
